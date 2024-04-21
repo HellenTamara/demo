b46df8a96870bb447782608c53e309a2cdba9542
Hellen Tamara <hellen.tamara@outlook.com>
2024-04-21 23:24:10 +0900

    filtering by year using lambda function and PHP built-in function

--- index.php
+++ index.php
@@ -12,68 +12,103 @@
   $books = [
     [
       'name' => 'Dark Matter',
-      'year' => '2016',
+      'year' => 2016,
       'author' => 'Philip K. Dick',
       'purchaseURL' => 'http://example.com'
     ],
     [
       'name' => 'Pride and Prejudice',
-      'year' => '1813',
+      'year' => 1813,
       'author' => 'Jane Austen',
       'purchaseURL' => 'http://example.com'
     ],
     [
       'name' => 'Sense and Sensibility',
-      'year' => '1811',
+      'year' => 1811,
       'author' => 'Jane Austen',
       'purchaseURL' => 'http://example.com'
     ],
     [
       'name' => 'Emma',
-      'year' => '1816',
+      'year' => 1816,
       'author' => 'Jane Austen',
       'purchaseURL' => 'http://example.com'
     ],
     [
       'name' => 'The little prince',
-      'year' => '1943',
+      'year' => 1943,
       'author' => ' Antoine de Saint-Exupéry',
       'purchaseURL' => 'http://example.com'
     ]
   ];
 
-  function filterByAuthor($books, $author)
+  // Regular Function
+  function filterRegular($items, $key, $value)
   {
     $filteredList = [];
-    foreach ($books as $book) {
-      if ($book['author'] === $author) {
-        $filteredList[] = $book;
+    foreach ($items as $item) {
+      if ($item[$key] === $value) {
+        $filteredList[] = $item;
       }
     }
     return $filteredList;
   }
+
+
+  // Lambda Function
+  // function filterLambda($items, $fn)
+  // {
+  //   $filteredList = [];
+  //   foreach ($items as $item) {
+  //     if ($fn($item)) {
+  //       $filteredList[] = $item;
+  //     }
+  //   }
+  //   return $filteredList;
+  // }
+
+  // $filteredBooks = filterLambda($books, function ($book) {
+  //   return $book['year'] < 2000;
+  // });
+
+
+  // Built in array filter
+  $filteredBooks = array_filter($books, function ($book) {
+    return $book['year'] < 2000;
+  });
 
   ?>
 
   <h3>Unfiltered List</h3>
   <ul>
     <?php foreach ($books as $book) { ?>
-        <li>
-          <a href="<?= $book['purchaseURL'] ?>">
-            <?= $book['name'] ?> <?= $book['year'] ?>
-          </a>
-        </li>
+      <li>
+        <a href="<?= $book['purchaseURL'] ?>">
+          <?= $book['name'] ?> <?= $book['year'] ?>
+        </a>
+      </li>
     <?php } ?>
   </ul>
 
   <h3>Filtered List (Jane Austen)</h3>
   <ul>
-    <?php foreach (filterByAuthor($books, 'Jane Austen') as $book) { ?>
-        <li>
-          <a href="<?= $book['purchaseURL'] ?>">
-            <?= $book['name'] ?> <?= $book['year'] ?>
-          </a>
-        </li>
+    <?php foreach (filterRegular($books, 'author', 'Jane Austen') as $book) { ?>
+      <li>
+        <a href="<?= $book['purchaseURL'] ?>">
+          <?= $book['name'] ?> <?= $book['year'] ?>
+        </a>
+      </li>
+    <?php } ?>
+  </ul>
+
+  <h3>Filtered List by Year</h3>
+  <ul>
+    <?php foreach ($filteredBooks as $book) { ?>
+      <li>
+        <a href="<?= $book['purchaseURL'] ?>">
+          <?= $book['name'] ?> <?= $book['year'] ?>
+        </a>
+      </li>
     <?php } ?>
   </ul>
 </body>
